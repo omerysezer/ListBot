1e368814593de2ada6aa4de8a0ff59ab1e9da9cc
FlammingOstrich <39203620+FlammingOstrich@users.noreply.github.com>
2021-05-31 16:51:12 -0400

    added command that allows user to specify their own nickname, and saves it into server settings

--- source/cogs/Commands.py
+++ source/cogs/Commands.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+import json
 
 
 class Commands(commands.Cog):
@@ -7,6 +8,18 @@ class Commands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.command()
+    @commands.guild_only()
+    async def change_name(self, ctx, name):
+        new_name = name
+        guild_key = str(ctx.guild.id)
+        with open('SERVER_SETTINGS.json', 'r') as file:
+            server_data = json.load(file)
+
+        server_data[guild_key][0][ctx.message.author.id] = name
+
+        with open('SERVER_SETTINGS.json', 'w') as file:
+            json.dump(server_data, file)
 
 def setup(bot):
     bot.add_cog(Commands(bot))
