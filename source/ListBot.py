167b5f507231eb65e98d84a6429f03f17b5a0195
FlammingOstrich <39203620+FlammingOstrich@users.noreply.github.com>
2021-05-31 14:38:52 -0400

    changes to BOT_TOKEN file

---
+++ source/ListBot.py
@@ -0,0 +1,14 @@
+import discord
+from discord.ext import commands
+import os
+
+intents = discord.Intents.default()
+intents.members = True
+bot = commands.Bot(command_prefix='=', intents=intents)
+
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        bot.load_extension(f"cogs.{filename[:-3]}")
+
+with open("BOT_TOKEN") as file:
+    bot.run(file.read())
